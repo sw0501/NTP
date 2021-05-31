2c87b9583d831ad37f092452655a3b52fc9a8a2a
sw0501 <dkxkqkrtkddn@gmail.com>
2021-05-31 05:53:02 +0000

    Time_Synch_Program 현재 PC의 시간을 실시간으로 받아오는 기능 추가

---
+++ Time_Synch_Program/Time_Synch_Program/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Time_Synch_Program
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PC_Hour = new System.Windows.Forms.TextBox();
+            this.PC_Min = new System.Windows.Forms.TextBox();
+            this.PC_Sec = new System.Windows.Forms.TextBox();
+            this.PC_Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // PC_Hour
+            // 
+            this.PC_Hour.Location = new System.Drawing.Point(47, 62);
+            this.PC_Hour.Name = "PC_Hour";
+            this.PC_Hour.Size = new System.Drawing.Size(115, 23);
+            this.PC_Hour.TabIndex = 0;
+            this.PC_Hour.Text = "PC_Hour";
+            // 
+            // PC_Min
+            // 
+            this.PC_Min.Location = new System.Drawing.Point(168, 62);
+            this.PC_Min.Name = "PC_Min";
+            this.PC_Min.Size = new System.Drawing.Size(100, 23);
+            this.PC_Min.TabIndex = 1;
+            this.PC_Min.Text = "PC_Min";
+            // 
+            // PC_Sec
+            // 
+            this.PC_Sec.Location = new System.Drawing.Point(274, 62);
+            this.PC_Sec.Name = "PC_Sec";
+            this.PC_Sec.Size = new System.Drawing.Size(100, 23);
+            this.PC_Sec.TabIndex = 2;
+            this.PC_Sec.Text = "PC_Sec";
+            // 
+            // PC_Timer
+            // 
+            this.PC_Timer.Tick += new System.EventHandler(this.PC_Timer_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 361);
+            this.Controls.Add(this.PC_Sec);
+            this.Controls.Add(this.PC_Min);
+            this.Controls.Add(this.PC_Hour);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox PC_Hour;
+        private System.Windows.Forms.TextBox PC_Min;
+        private System.Windows.Forms.TextBox PC_Sec;
+        private System.Windows.Forms.Timer PC_Timer;
+    }
+}
+
---
+++ Time_Synch_Program/Time_Synch_Program/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;  //기본 패키지에는 제공되어있지 않아서 Nuget 패키지에서 설치해야됨
+
+namespace Time_Synch_Program
+{
+    public partial class Main : Form
+    {
+        //SerialPort m_sp1';
+
+        public Main()
+        {
+            InitializeComponent();
+
+            //Timer를 이용하여 현재 PC의 시간을 계속해서 갱신한다.
+            PC_Timer.Enabled = true;
+            PC_Timer.Tick += PC_Timer_Tick;
+        }
+
+        private void PC_Timer_Tick(object sender, EventArgs e)
+        {
+            //현재 PC의 시간정보를 문자열 형식으로 변환하여 텍스트에 넣어줌 -> 텍스트 아니어도 라벨에 넣으면 더 이쁠듯
+            PC_Hour.Text = System.DateTime.Now.ToString("HH");
+            PC_Min.Text = System.DateTime.Now.ToString("mm");
+            PC_Sec.Text = System.DateTime.Now.ToString("ss");
+        }
+    }
+}
