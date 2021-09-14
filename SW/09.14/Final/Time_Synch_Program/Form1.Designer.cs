94f482624a0d0ff8029d4586f99c8dfe431646ac
sw0501 <dkxkqkrtkddn@gmail.com>
2021-09-14 08:38:10 +0000

    지연시간 기능 추가 및 포트이름과 BaudRate 저장

---
+++ SW/09.14/Final/Time_Synch_Program/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿
+namespace Time_Synch_Program
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PC_Timer = new System.Windows.Forms.Timer(this.components);
+            this.Serial_Port = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BaudRate = new System.Windows.Forms.TextBox();
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Intranet_Server_Time = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Port_Connect_Status = new System.Windows.Forms.PictureBox();
+            this.Sync_Status = new System.Windows.Forms.PictureBox();
+            this.TX_Status = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Voip_Server_Time = new System.Windows.Forms.Label();
+            this.Delayed_Time = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Port_Connect_Status)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Sync_Status)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TX_Status)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PC_Timer
+            // 
+            this.PC_Timer.Tick += new System.EventHandler(this.PC_Timer_Tick);
+            // 
+            // Serial_Port
+            // 
+            this.Serial_Port.Font = new System.Drawing.Font("맑은 고딕", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Serial_Port.Location = new System.Drawing.Point(472, 317);
+            this.Serial_Port.Name = "Serial_Port";
+            this.Serial_Port.Size = new System.Drawing.Size(100, 43);
+            this.Serial_Port.TabIndex = 3;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label.Location = new System.Drawing.Point(375, 317);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(91, 37);
+            this.label.TabIndex = 4;
+            this.label.Text = "Port : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(375, 363);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 37);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Baud : ";
+            // 
+            // BaudRate
+            // 
+            this.BaudRate.Font = new System.Drawing.Font("맑은 고딕", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BaudRate.Location = new System.Drawing.Point(472, 366);
+            this.BaudRate.Name = "BaudRate";
+            this.BaudRate.Size = new System.Drawing.Size(100, 43);
+            this.BaudRate.TabIndex = 6;
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_Open.Location = new System.Drawing.Point(52, 350);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(100, 50);
+            this.btn_Open.TabIndex = 7;
+            this.btn_Open.Text = "Open";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_Close.Location = new System.Drawing.Point(188, 350);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(100, 50);
+            this.btn_Close.TabIndex = 8;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-13, -185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("배달의민족 주아", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(18, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(270, 53);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "인트라넷 서버";
+            // 
+            // Intranet_Server_Time
+            // 
+            this.Intranet_Server_Time.AutoSize = true;
+            this.Intranet_Server_Time.BackColor = System.Drawing.SystemColors.Control;
+            this.Intranet_Server_Time.Font = new System.Drawing.Font("배달의민족 주아", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Intranet_Server_Time.Location = new System.Drawing.Point(305, 48);
+            this.Intranet_Server_Time.Name = "Intranet_Server_Time";
+            this.Intranet_Server_Time.Size = new System.Drawing.Size(680, 53);
+            this.Intranet_Server_Time.TabIndex = 13;
+            this.Intranet_Server_Time.Text = "Intranet_Server_Time_Delayed";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("맑은 고딕", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(676, 304);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 28);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "포트 연결 상태";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("맑은 고딕", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(676, 365);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 28);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "동기화 상태";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("맑은 고딕", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(676, 432);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(146, 28);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "수신 여부 확인";
+            // 
+            // Port_Connect_Status
+            // 
+            this.Port_Connect_Status.Image = global::Time_Synch_Program.Properties.Resources.RC1;
+            this.Port_Connect_Status.Location = new System.Drawing.Point(832, 284);
+            this.Port_Connect_Status.Name = "Port_Connect_Status";
+            this.Port_Connect_Status.Size = new System.Drawing.Size(60, 60);
+            this.Port_Connect_Status.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Port_Connect_Status.TabIndex = 19;
+            this.Port_Connect_Status.TabStop = false;
+            // 
+            // Sync_Status
+            // 
+            this.Sync_Status.Image = global::Time_Synch_Program.Properties.Resources.RC1;
+            this.Sync_Status.Location = new System.Drawing.Point(832, 350);
+            this.Sync_Status.Name = "Sync_Status";
+            this.Sync_Status.Size = new System.Drawing.Size(60, 60);
+            this.Sync_Status.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Sync_Status.TabIndex = 20;
+            this.Sync_Status.TabStop = false;
+            // 
+            // TX_Status
+            // 
+            this.TX_Status.Image = global::Time_Synch_Program.Properties.Resources.RC1;
+            this.TX_Status.Location = new System.Drawing.Point(832, 416);
+            this.TX_Status.Name = "TX_Status";
+            this.TX_Status.Size = new System.Drawing.Size(60, 60);
+            this.TX_Status.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.TX_Status.TabIndex = 21;
+            this.TX_Status.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.Control;
+            this.label8.Font = new System.Drawing.Font("배달의민족 주아", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(84, 153);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(204, 53);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Voip 서버";
+            // 
+            // Voip_Server_Time
+            // 
+            this.Voip_Server_Time.AutoSize = true;
+            this.Voip_Server_Time.BackColor = System.Drawing.SystemColors.Control;
+            this.Voip_Server_Time.Font = new System.Drawing.Font("배달의민족 주아", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Voip_Server_Time.Location = new System.Drawing.Point(305, 153);
+            this.Voip_Server_Time.Name = "Voip_Server_Time";
+            this.Voip_Server_Time.Size = new System.Drawing.Size(406, 53);
+            this.Voip_Server_Time.TabIndex = 23;
+            this.Voip_Server_Time.Text = "Voip_Server_Time";
+            // 
+            // Delayed_Time
+            // 
+            this.Delayed_Time.Font = new System.Drawing.Font("맑은 고딕", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Delayed_Time.Location = new System.Drawing.Point(472, 415);
+            this.Delayed_Time.Name = "Delayed_Time";
+            this.Delayed_Time.Size = new System.Drawing.Size(100, 43);
+            this.Delayed_Time.TabIndex = 25;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(375, 408);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(99, 37);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Delay :";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 501);
+            this.Controls.Add(this.Delayed_Time);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.Voip_Server_Time);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.TX_Status);
+            this.Controls.Add(this.Sync_Status);
+            this.Controls.Add(this.Port_Connect_Status);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.btn_Open);
+            this.Controls.Add(this.BaudRate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.Serial_Port);
+            this.Controls.Add(this.Intranet_Server_Time);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Port_Connect_Status)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Sync_Status)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TX_Status)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer PC_Timer;
+        private System.Windows.Forms.TextBox Serial_Port;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox BaudRate;
+        private System.Windows.Forms.Button btn_Open;
+        private System.Windows.Forms.Button btn_Close;
+        //private System.Windows.Forms.Button btn_RX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Intranet_Server_Time;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox Port_Connect_Status;
+        private System.Windows.Forms.PictureBox Sync_Status;
+        private System.Windows.Forms.PictureBox TX_Status;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label Voip_Server_Time;
+        private System.Windows.Forms.TextBox Delayed_Time;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ SW/09.14/Final/Time_Synch_Program/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;                  //기본 패키지에는 제공되어있지 않아서 Nuget 패키지에서 설치해야됨
+using System.Runtime.InteropServices;   //시스템 시간 변경을 위해서 참조
+using System.Diagnostics;               //디버깅용
+
+namespace Time_Synch_Program
+{
+    public partial class Main : Form
+    {
+        SerialPort m_sp1;   //시리얼포트 변수
+
+        public struct SYSTEMTIME    //public 선언안하고 쓰면 CS0051 오류 발생
+        {
+            public ushort wYear; 
+            public ushort wMonth; 
+            public ushort wDayOfWeek;
+            public ushort wDay; 
+            public ushort wHour; 
+            public ushort wMinute; 
+            public ushort wSecond;
+            public ushort wMilliseconds;
+        }
+
+        [DllImport("kernel32.dll",SetLastError = true)] //SetSystemTime API를 이용하기 위한 연관 시스템 파일
+        public extern static bool SetSystemTime(ref SYSTEMTIME st); //시스템 시간을 변경하는 API
+
+        public Main()
+        {
+            InitializeComponent();
+
+            //Timer를 이용하여 현재 PC의 시간을 계속해서 갱신한다.
+            PC_Timer.Enabled = true;
+            PC_Timer.Tick += PC_Timer_Tick;
+
+        }
+
+        //시간이 지날때마다 PC_Timer_Tick 가 갱신됨
+        private void PC_Timer_Tick(object sender, EventArgs e)
+        {
+            //현재 PC의 시간정보 갱신 -> 동기화가 풀려도 PC 자체 클럭으로 돌아감
+            Voip_Server_Time.Text = System.DateTime.Now.ToString();
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            //사용자가 세팅에서 Port 이름과 BaudRate 입력하고 그 값을 받아와야함
+            //전에 사용했던 값 입력 받으면 될 듯
+            Serial_Port.Text = "COM5";
+            BaudRate.Text = "9600";
+            Delayed_Time.Text = Properties.Settings.Default.Delay.ToString();
+
+
+            //속성에 저장되어있는 Last_Server_Time을 불러와서 Text에 저장
+            Intranet_Server_Time.Text = Properties.Settings.Default.Last_Server_Time.ToString();
+
+            //초기 상태 표시
+            Port_Connect_Status.Image = Properties.Resources.RC1;
+            Sync_Status.Image = Properties.Resources.RC1;
+            TX_Status.Image = Properties.Resources.RC1;
+        }
+
+        private void btn_Open_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //포트이름이 비어있거나 바우드레이트가 입력이 안되어있는 경우 오류 메세지 표시
+                if (Serial_Port.Text.Equals("") || BaudRate.Text.Equals("") || Convert.ToInt32(BaudRate.Text.ToString())<=0)
+                {
+                    MessageBox.Show("올바른 Port와 Baud를 입력해주세요");
+                }
+                else
+                {
+                    if (null == m_sp1)
+                    {
+                        //포트가 열렸을 경우
+                        Port_Connect_Status.Image = Properties.Resources.GC1;
+                        Sync_Status.Image = Properties.Resources.GC1;
+                        TX_Status.Image = Properties.Resources.YC1;
+
+                        m_sp1 = new SerialPort();
+                        m_sp1.PortName = Serial_Port.Text;    //컴포트명
+                        m_sp1.BaudRate = Convert.ToInt32(BaudRate.Text); //바우드레이트
+                        m_sp1.Parity = Parity.None;
+                        m_sp1.DataBits = 8;
+                        m_sp1.StopBits = StopBits.One;
+
+                        //포트에 남아있는 데이터를 읽어서 처리
+                        m_sp1.DataReceived += new SerialDataReceivedEventHandler(EventDataReceived);
+                        m_sp1.Open();
+                        
+                    }
+
+                    btn_Open.Enabled = false;   //Open Button Disable
+                    btn_Close.Enabled = true;    //Close Button Enable
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+
+            if (null != m_sp1)
+            {
+                if (m_sp1.IsOpen)
+                {
+                    //포트가 닫혔을 경우
+                    Port_Connect_Status.Image = Properties.Resources.RC1;
+                    Sync_Status.Image = Properties.Resources.RC1;
+                    TX_Status.Image = Properties.Resources.RC1;
+
+                    m_sp1.Close();
+                    m_sp1.Dispose();
+                    m_sp1 = null;
+                }
+                btn_Open.Enabled = true;    //Open Button Enable
+                btn_Close.Enabled = false;  //Close Button Disable
+            }
+
+
+        }
+
+        private void EventDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+        
+            int iRecSize = m_sp1.BytesToRead; // 수신된 데이터 갯수
+            string strRxData;   //수신 데이터 문자열
+
+            try
+            {
+                if (iRecSize != 0) // 수신된 데이터의 수가 0이 아닐때만 처리하자
+                {
+                    strRxData = "";
+
+                    //이벤트가 발생할때마다 m_spl에 저장된 수신데이터를 저장
+                    strRxData = m_sp1.ReadExisting();
+
+                    //저장된 수신데이터에서 원하는 값을 추출
+                    pharsingMsg(strRxData);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            //이벤트가 끝난 경우 대기상태 표시
+            TX_Status.Image = Properties.Resources.YC1;
+
+        }
+
+        //수신된 GPS 데이터에서 원하는 데이터 추출
+        private void pharsingMsg(string Msg)
+        {
+            int GPS_year = 0;           //연도
+            int GPS_month = 0;          //월
+            int GPS_day = 0;            //일
+            int GPS_hour = 0;           //시
+            int GPS_minute = 0;         //분
+            int GPS_second = 0;         //초
+            int GPS_milliseconds = 0;   //밀리초
+
+
+            string[] ChkMsg = null;     //
+            string temp = "";           //
+
+            //Msg가 비어있거나 공백인 경우 추출하지 않는다
+            if(Msg == null || Msg.Equals(""))
+            {
+
+            }
+            else
+            {
+                try
+                {
+                    Msg.Replace("\n\r", "");
+                    int selectIDX = -1;
+                    string[] strArr = Msg.Split('$');
+                    //최근의 GPRMC 문자열 찾기
+                    for(int i = strArr.Length - 1; i >= 0; i--)
+                    {
+                        if (strArr[i].ToString().Substring(0, 5).Equals("GPRMC"))
+                        {
+                            //수신 문자열이 유효하고 필요한 정보를 가지고 있는지 확인
+                            ChkMsg = strArr[i].ToString().Split(',');
+                            if(ChkMsg[2] == null || ChkMsg[3] == null)
+                            {
+                                continue;
+                            }
+                            else if(ChkMsg[2].ToString().Replace(" ","").Equals("")|| ChkMsg[3].ToString().Replace(" ", "").Equals(""))
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                selectIDX = i;
+                                break;
+                            }
+                        }
+                    }
+
+                    //수신 문자열중 GPRMC가 없을 때 기존 값들 초기화
+                    if(selectIDX < 0)
+                    {
+                        GPS_year = 0;          
+                        GPS_month = 0;
+                        GPS_day = 0;           
+                        GPS_hour = 0;          
+                        GPS_minute = 0;         
+                        GPS_second = 0;        
+                        GPS_milliseconds = 0;
+                    }
+                    else
+                    {
+                        //이벤트가 발생했을 상태 표시
+                        TX_Status.Image = Properties.Resources.GC1;
+
+                        string[] ArrMsg = ChkMsg;
+                        temp = ArrMsg[1].ToString();    //시간데이터
+
+                        //SetSystemTime 이 그리니치 표준시를 사용하기 때문에 시간조정 필요 X
+                        GPS_hour = Convert.ToInt16(temp.Substring(0, 2));
+                        
+                        if (GPS_hour > 24)
+                        {
+                            GPS_hour = GPS_hour - 24;
+                        }
+
+                        //string 대신 datatime 형식으로 변환해서 사용하면 될 듯
+                        GPS_minute = Convert.ToInt16(temp.Substring(2, 2));
+                        GPS_second = Convert.ToInt16(temp.Substring(4, 2));
+                        GPS_milliseconds = Convert.ToInt16(temp.Substring(7, 3));
+                        GPS_year = 2000 + Convert.ToInt16(ArrMsg[9].Substring(4, 2));
+                        GPS_month = Convert.ToInt16(ArrMsg[9].Substring(2, 2));
+                        GPS_day = Convert.ToInt16(ArrMsg[9].Substring(0, 2));
+
+                        //tmpdata 안에 변경할 시간 정보들을 변수로 넘겨주면 된다.
+                        DateTime GPS_Time = new DateTime(GPS_year,GPS_month,GPS_day,GPS_hour,GPS_minute,GPS_second,GPS_milliseconds);
+
+                        //그리니치 표준시 적용을 위해 + 9H
+                        //Last_Server_Time에 가장 최근에 받은 GPS_Time 저장 후 설정 저장
+                        Properties.Settings.Default.Last_Server_Time = GPS_Time.AddHours(9);
+
+                        //지연시간 저장
+                        Properties.Settings.Default.Delay = Convert.ToInt16(Delayed_Time.Text);
+
+                        //SetSystemTime이 한국시간 변환을 위해 자동으로 9H 더해주기때문에 그냥 지연시간만 더해주면 됨 (나중에 필요할때 millisecond로 변경하면 됨)
+                        Properties.Settings.Default.Last_Server_Time_Delayed = GPS_Time.AddSeconds(Properties.Settings.Default.Delay);
+
+                        //사용자가 입력한 지연시간만큼 더한후에 저장
+                        Properties.Settings.Default.Save(); //설정 저장
+
+                        //이벤트가 생기면 갱신
+                        Intranet_Server_Time.Text = Properties.Settings.Default.Last_Server_Time.ToString();
+                        
+                        //SetSystemTime을 이용하기 위해 선언 후 Last_Server_Time_Delayed 데이터 저장
+                        SYSTEMTIME st = new SYSTEMTIME();
+
+                        st.wYear = (ushort)Properties.Settings.Default.Last_Server_Time_Delayed.Year;
+                        st.wDayOfWeek = (ushort)Properties.Settings.Default.Last_Server_Time_Delayed.DayOfWeek;
+                        st.wMonth = (ushort)Properties.Settings.Default.Last_Server_Time_Delayed.Month;
+                        st.wDay = (ushort)Properties.Settings.Default.Last_Server_Time_Delayed.Day;
+                        st.wHour = (ushort)Properties.Settings.Default.Last_Server_Time_Delayed.Hour;
+                        st.wMinute = (ushort)Properties.Settings.Default.Last_Server_Time_Delayed.Minute;
+                        st.wSecond = (ushort)Properties.Settings.Default.Last_Server_Time_Delayed.Second;
+                        st.wMilliseconds = (ushort)Properties.Settings.Default.Last_Server_Time_Delayed.Millisecond;
+
+
+                        //컴퓨터의 시간 변경
+                        bool E = SetSystemTime(ref st);
+
+                        //에러 체크 -> 1314 : 관리자 권한 -> 빌드하고 관리자 권한으로 열기하면 됨
+                        if (E == false)
+                        {
+                            int lastError = Marshal.GetLastWin32Error();
+                            Debug.WriteLine(lastError.ToString());
+                        }
+
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
